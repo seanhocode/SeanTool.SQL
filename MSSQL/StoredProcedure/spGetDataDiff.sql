b7f9e30488feece1ec3284286d593dccce7cb92a
sean_ho <sean_ho@gss.com.tw>
2026-03-19 11:49:08 +0800

    [Title] Initial [Feat] - 新增sp:spGetDataDiff

---
+++ MSSQL/StoredProcedure/spGetDataDiff.sql
@@ -0,0 +1,229 @@
+DROP PROCEDURE [dbo].[spGetDataDiff]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE    	PROCEDURE  [dbo].[spGetDataDiff]
+/*
+-- =============================================
+-- Create date: <yyyy.mm.dd>
+-- Description:
+@ColumnMapping 格式：[DS1欄位名稱]:[DS2欄位名稱],[DS1欄位名稱]:[DS2欄位名稱]
+@ColumnMapping 兩邊皆對應的到欄位才會納入比對，若有指定但找不到欄位則忽略該欄位
+@IsWarningNotExist 是否要顯示只存在於其中一個資料來源的欄位
+@KeyColumns 可指定主鍵欄位，若未指定則自動帶入 PK 欄位，自動帶入PK時若兩邊PK欄位不同則會同時帶入兩邊的PK欄位
+@IgnoreColumns 只可忽略非Key欄位
+
+-- Ex.
+        SELECT * INTO SystemRepository_ST FROM SystemRepository
+        UPDATE SystemRepository_ST SET [Value] = 'Test' WHERE [Value] = 'MTQ'
+
+        EXEC dbo.spGetDataDiff
+            @DataSource1 = 'SystemRepository',
+            @DataSource2 = 'SystemRepository_ST',
+            @KeyColumns = NULL,
+            @IgnoreColumns = NULL,
+            @ColumnMapping = NULL,
+            @IsWarningNotExist = 1
+
+        DROP TABLE SystemRepository_ST
+-- =============================================
+*/
+    @DataSource1            VARCHAR (MAX)
+    , @DataSource2          VARCHAR (MAX)
+    , @KeyColumns           VARCHAR (MAX)
+    , @IgnoreColumns        VARCHAR (MAX)
+    , @ColumnMapping        VARCHAR (MAX)
+    , @IsWarningNotExist    BIT = 1
+AS
+BEGIN
+    DECLARE @ColumnInfo         TABLE
+    (
+        DS1ColName      VARCHAR (MAX)
+        , DS2ColName    VARCHAR (MAX)
+        , IsKey         BIT DEFAULT 0
+    )
+    DECLARE @KeyConditionSQL    VARCHAR (MAX) = ''
+    DECLARE @ColumnDiffSQL      VARCHAR (MAX) = ''
+    DECLARE @CompareSQL         VARCHAR (MAX) = 
+'
+SELECT [KeyColumns]
+    , Diff.ColName AS [DiffColumn]
+    , Diff.DS1Value AS [DataSource1]_Value
+    , Diff.DS2Value AS [DataSource2]_Value
+FROM [DataSource1] DS1
+JOIN [DataSource2] DS2
+    ON [KeyConditionSQL]
+CROSS APPLY(
+    [ColumnDiffSQL]
+) AS Diff (ColName, DS1Value, DS2Value)
+'
+
+    --#region 處理 Key Columns
+    --========================================取的欄位資訊========================================
+    -- 加入對應欄位
+    INSERT INTO @ColumnInfo (DS1ColName, DS2ColName)
+    SELECT
+        LEFT(C.Item, C.Pos - 1) AS Col1
+        , SUBSTRING(C.Item, C.Pos + 1, LEN(C.Item)) AS Col2
+    FROM (
+        SELECT 
+            [value] AS Item, 
+            CHARINDEX(':', [value]) AS Pos
+        FROM string_split(@ColumnMapping, ',')
+    ) AS C
+    WHERE C.Pos > 0 -- 有冒號才處理
+        -- 確認兩邊欄位都存在
+        AND EXISTS( 
+            SELECT 1
+            FROM INFORMATION_SCHEMA.COLUMNS X
+            WHERE X.TABLE_NAME = @DataSource1
+                AND X.COLUMN_NAME = LEFT(C.Item, C.Pos - 1)
+        )
+        AND EXISTS(
+            SELECT 1
+            FROM INFORMATION_SCHEMA.COLUMNS Y
+            WHERE Y.TABLE_NAME = @DataSource2
+                AND Y.COLUMN_NAME = SUBSTRING(C.Item, C.Pos + 1, LEN(C.Item))
+        )
+
+    -- 加入相同欄位
+    INSERT INTO @ColumnInfo (DS1ColName, DS2ColName)
+    SELECT DISTINCT C.COLUMN_NAME, C.COLUMN_NAME
+    FROM INFORMATION_SCHEMA.COLUMNS C
+    WHERE C.TABLE_NAME = @DataSource1
+        AND EXISTS(
+            SELECT 1
+            FROM INFORMATION_SCHEMA.COLUMNS X
+            WHERE X.TABLE_NAME = @DataSource2
+                AND X.COLUMN_NAME = C.COLUMN_NAME
+        )
+
+    -- 加入只存在於其中一個資料來源的欄位
+    IF(@IsWarningNotExist = 1)
+    BEGIN
+        INSERT INTO @ColumnInfo (DS1ColName, DS2ColName)
+        SELECT C.COLUMN_NAME, NULL
+        FROM INFORMATION_SCHEMA.COLUMNS C
+        WHERE C.TABLE_NAME = @DataSource1
+            AND NOT EXISTS(
+                SELECT 1
+                FROM @ColumnInfo CI
+                WHERE C.COLUMN_NAME = CI.DS1ColName
+            )
+
+        INSERT INTO @ColumnInfo (DS1ColName, DS2ColName)
+        SELECT NULL, C.COLUMN_NAME
+        FROM INFORMATION_SCHEMA.COLUMNS C
+        WHERE C.TABLE_NAME = @DataSource2
+            AND NOT EXISTS(
+                SELECT 1
+                FROM @ColumnInfo CI
+                WHERE C.COLUMN_NAME = CI.DS2ColName
+            )
+    END
+
+    -- 標記 Key 欄位
+    UPDATE CI
+    SET IsKey = 1
+    FROM @ColumnInfo CI
+    WHERE CI.DS1ColName IS NOT NULL
+        AND CI.DS2ColName IS NOT NULL
+        AND EXISTS(
+            SELECT 1
+            FROM string_split(@KeyColumns, ',') X
+            WHERE CI.DS1ColName = X.[value]
+                OR CI.DS2ColName = X.[value]
+        )
+
+    -- 若沒有指定 Key 欄位，則自動帶入 PK 欄位
+    IF NOT EXISTS(SELECT 1 FROM @ColumnInfo WHERE IsKey = 1)
+    BEGIN
+        UPDATE CI
+        SET IsKey = 1
+        FROM @ColumnInfo CI
+        WHERE CI.DS1ColName IS NOT NULL
+            AND CI.DS2ColName IS NOT NULL
+            AND EXISTS(
+                SELECT 1
+                FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS TC
+                JOIN INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE CU 
+                    ON TC.CONSTRAINT_NAME = CU.CONSTRAINT_NAME
+                WHERE TC.TABLE_NAME = @DataSource1
+                    AND TC.CONSTRAINT_TYPE = 'PRIMARY KEY'
+                    AND CI.DS1ColName = CU.COLUMN_NAME
+            )
+
+        UPDATE CI
+        SET IsKey = 1
+        FROM @ColumnInfo CI
+        WHERE CI.DS1ColName IS NOT NULL
+            AND CI.DS2ColName IS NOT NULL
+            AND EXISTS(
+                SELECT 1
+                FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS TC
+                JOIN INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE CU 
+                    ON TC.CONSTRAINT_NAME = CU.CONSTRAINT_NAME
+                WHERE TC.TABLE_NAME = @DataSource2
+                    AND TC.CONSTRAINT_TYPE = 'PRIMARY KEY'
+                    AND CI.DS2ColName = CU.COLUMN_NAME
+            )
+    END
+
+    -- 移除忽略欄位
+    DELETE CI
+    FROM @ColumnInfo CI
+    WHERE CI.IsKey = 0
+        AND EXISTS(
+            SELECT 1
+            FROM string_split(@IgnoreColumns, ',') X
+            WHERE CI.DS1ColName = X.[value]
+                OR CI.DS2ColName = X.[value]
+        )
+    --========================================取的欄位資訊========================================
+    
+    --#endregion 處理 Key Columns
+    
+    --#region 組出腳本語法
+    --========================================組出腳本語法========================================
+    SELECT @KeyConditionSQL = @KeyConditionSQL
+        + '    AND DS1.' + DS1ColName + ' = DS2.' + DS2ColName
+        + CHAR (13) + CHAR (10)
+    FROM @ColumnInfo
+    WHERE IsKey = 1
+    SET @KeyConditionSQL = STUFF(@KeyConditionSQL, 1, 8, '')
+
+    SELECT @ColumnDiffSQL = @ColumnDiffSQL
+        + CHAR (13) + CHAR (10) + '    '
+        + 'UNION SELECT ''' + ISNULL(CI.DS1ColName, CI.DS2ColName) + ''''
+        + CASE WHEN CI.DS1ColName IS NOT NULL THEN ', CAST(DS1.' + CI.DS1ColName + ' AS VARCHAR)' ELSE ', NULL' END
+        + CASE WHEN CI.DS2ColName IS NOT NULL THEN ', CAST(DS2.' + CI.DS2ColName + ' AS VARCHAR)' ELSE ', NULL' END
+        + CASE 
+            WHEN (CI.DS1ColName IS NOT NULL AND CI.DS2ColName IS NOT NULL)
+                THEN ' WHERE DS1.' + ISNULL(CI.DS1ColName, '') + ' <> DS2.' + ISNULL(CI.DS2ColName, '')
+            ELSE ''
+        END
+    FROM @ColumnInfo CI
+    WHERE IsKey = 0
+    SET @ColumnDiffSQL = STUFF(@ColumnDiffSQL, 1, 12, '')
+
+    SET @CompareSQL = REPLACE(@CompareSQL, '[DataSource1]', @DataSource1)
+    SET @CompareSQL = REPLACE(@CompareSQL, '[DataSource2]', @DataSource2)
+    SET @CompareSQL = REPLACE(@CompareSQL, '[ColumnDiffSQL]', @ColumnDiffSQL)
+    SET @CompareSQL = REPLACE(@CompareSQL, '[KeyConditionSQL]', @KeyConditionSQL)
+    SET @CompareSQL = REPLACE(@CompareSQL, '[KeyColumns]', (SELECT STRING_AGG( 'DS1.' + DS1ColName, ',') FROM @ColumnInfo WHERE IsKey = 1))
+    -- SET @CompareSQL = REPLACE(
+    --         @CompareSQL
+    --         , '[KeyColumns]'
+    --         , (
+    --             SELECT STRING_AGG('''' + T.DS1ColName + '='' + CAST(DS1.' + T.DS1ColName + ' AS VARCHAR) + '',''', ' + ') 
+    --             FROM (SELECT DISTINCT DS1ColName FROM @ColumnInfo WHERE IsKey = 1) T
+    --         )
+    --     )
+    --========================================組出腳本語法========================================
+    --#endregion 組出腳本語法
+
+    EXEC(@CompareSQL)
+    PRINT @CompareSQL
+END
